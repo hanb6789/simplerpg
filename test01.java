8f89ca63254be413e9aaf24b09c3b67d5a004b87
hanb147258369 <hanb147258369@gmail.com>
2020-09-04 15:22:50 +0800

    1nd version

---
+++ test01.java
@@ -0,0 +1,25 @@
+package test;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private String food;
+		private int lifenumber;
+		int a = 3, b = 4;
+		if ( a++ >= 3 || b-- <= 3){
+		System.out.println("1:" + a + ":"+ b);
+		}else {
+		System.out.println("2:" + a + ":"+ b);
+		}
+		final double defaultFortune = 1500000;
+		double interestRate = 0.02;
+		double totalFortune = defaultFortune;
+		for(int i=0;i<10;i++) {
+			totalFortune = totalFortune + (totalFortune*interestRate);
+		}
+		System.out.println("第五題");
+		System.out.println("10年後本金加利息共有" + totalFortune + "元");//利率
+	}
+
+}
